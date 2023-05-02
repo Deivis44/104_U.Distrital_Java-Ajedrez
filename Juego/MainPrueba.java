53b41b9a2e3422bdbe363fcafc3502307fe6cb4b
Deivis44 <david20041124@gmail.com>
2023-05-02 15:00:12 -0500

    starting beta

---
+++ Juego/MainPrueba.java
@@ -0,0 +1,10 @@
+package Juego;
+
+public class MainPrueba {
+    
+    public static void main(String[] args) {
+        
+        Prueba ventana = new Prueba();
+        ventana.setVisible(true);
+    }
+}
