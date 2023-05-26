57796ae810a527bae1bba3fdc2647d433ceb0363
Deivis44 <david20041124@gmail.com>
2023-05-26 16:05:37 -0500

    adding sound effects

--- Chess/src/Main/Input.java
+++ Chess/src/Main/Input.java
@@ -2,19 +2,39 @@ package Main;
 
 import Pieces.Piece;
 
+import javax.sound.sampled.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 
 public class Input extends MouseAdapter {
     
     Board board;
+    Clip clickSound; // Clip para el sonido de clic;
+    Clip errorSound; // Clip para el sonido de error
+
     public Input(Board board) {
         this.board = board;
+
+        try {
+            // Obtener el archivo de audio del sonido de clic
+            AudioInputStream clickInputStream = AudioSystem.getAudioInputStream(Input.class.getResourceAsStream("/musica/efectos/click.wav"));
+            // Cargar el clip de audio para el sonido de clic
+            clickSound = AudioSystem.getClip();
+            clickSound.open(clickInputStream);
+
+            // Obtener el archivo de audio del sonido de error
+            AudioInputStream errorInputStream = AudioSystem.getAudioInputStream(Input.class.getResourceAsStream("/musica/efectos/error.wav"));
+            // Cargar el clip de audio para el sonido de error
+            errorSound = AudioSystem.getClip();
+            errorSound.open(errorInputStream);
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void mousePressed(MouseEvent e) {
-
         int col = e.getX() / board.tileSize;
         int row = e.getY() / board.tileSize;
 
@@ -22,6 +42,7 @@ public class Input extends MouseAdapter {
 
         if (pieceXY != null) {
             board.selectedPiece = pieceXY;
+            reproducirSonido(clickSound);
         }
     }
 
@@ -31,26 +52,35 @@ public class Input extends MouseAdapter {
     int contador = 1;
     @Override
     public void mouseReleased(MouseEvent e) {
-        
         int col = e.getX() / board.tileSize;
         int row = e.getY() / board.tileSize;
 
         if (board.selectedPiece != null) {
             Move move = new Move(board, board.selectedPiece, col, row);
-            
+
             if (board.isValidMove(move) && ((turnoBlancas && board.selectedPiece.isWhite) || (!turnoBlancas && !board.selectedPiece.isWhite))) {
                 board.makeMove(move);
                 System.out.println((contador++) + " Movimiento válido");
-                turnoBlancas = !turnoBlancas; 
+                turnoBlancas = !turnoBlancas;
+                reproducirSonido(clickSound);
             } else {
                 System.out.println((contador++) + " Movimiento inválido");
                 board.selectedPiece.xPos = board.selectedPiece.col * board.tileSize;
                 board.selectedPiece.yPos = board.selectedPiece.row * board.tileSize;
+                reproducirSonido(errorSound);
             }
         }
-        
+
         board.selectedPiece = null;
         board.repaint();
+    }
+
+    private void reproducirSonido(Clip clip) {
+        if (clip != null) {
+            clip.stop(); // Detener el sonido si se está reproduciendo
+            clip.setFramePosition(0); // Reiniciar la reproducción desde el inicio
+            clip.start(); // Reproducir el sonido
+        }
     }
     
 
