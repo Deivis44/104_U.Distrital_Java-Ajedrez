970bb861b265c3838d6d3e0cf426995415cf483c
Deivis44 <david20041124@gmail.com>
2023-05-27 00:33:21 -0500

    delete unnecessary items

--- A/Juego/pruebas.java
+++
@@ -1,5 +0,0 @@
-package A.Juego;
-
-public class pruebas {
-    
-}
--- A/Juego/pruebasMain.java
+++
@@ -1,8 +0,0 @@
-package A.Juego;
-
-public class pruebasMain {
-    
-    public static void main(String[] args) {
-    }        
-
-}
